d922ff5b00bc95c6b1ce3a340ec352c3305bdf7e
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-05-24 16:34:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  return parseInt('n')
+  return parseInt(n)
   }
   
 
 function preserveDecimal(n) { 
-    return parseFloat(`n`)
+    return parseFloat(n)
   }
