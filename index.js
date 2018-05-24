1120356d78c5d0eb06a8305c09b6c81f3a888d3f
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-05-24 16:19:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function dec(n) {
   return n -= 1;
 }
 
-function makeInt() {
+function makeInt(n) {
     var a = parseInt ('n');
     var b = parseInt ('n', 10);
     var c = isNaN(parseInt('sldkjflksjf'));
