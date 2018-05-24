9d4b3335ae084bba0f43523986500c3d35d3e679
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-05-24 15:53:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ function makeInt(n, base) {
   if (n === a.toString) {
     return parseInt(a)
   }
-  else if (n === ) {
-    return parseInt
+  else if (n === 0x ) {
+    return parseInt(n, base)
   }
   else {
     return parseInt('NaN')
